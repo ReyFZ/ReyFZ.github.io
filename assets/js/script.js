7f5d861f908c8a32a1d88fac40969ca33d7f97b6
ReyFZ <118872774+ReyFZ@users.noreply.github.com>
2023-10-09 22:30:30 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,20 +141,24 @@ for (let i = 0; i < formInputs.length; i++) {
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
+// Add event to all nav links
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
+    for (let j = 0; j < pages.length; j++) {
+      if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
+        pages[j].classList.add("active");
+        if (navigationLinks[j]) { // Check if navigationLink exists
+          navigationLinks[j].classList.add("active");
+        }
         window.scrollTo(0, 0);
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[j].classList.remove("active");
+        if (navigationLinks[j]) { // Check if navigationLink exists
+          navigationLinks[j].classList.remove("active");
+        }
       }
     }
-
   });
 }
+
